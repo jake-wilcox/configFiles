b8886a5d670df4158bd93dbcafaac5c956eed3fc
Jake Wilcox <jakewilcox242@gmail.com>
2023-04-03 20:06:50 -0700

    added a few plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,13 @@ require("jake.plugins-setup")
 require("jake.core.options")
 require("jake.core.keymaps")
 require("jake.core.colorscheme")
+require("jake.plugins.comment")
+require("jake.plugins.nvim-tree")
+require("jake.plugins.lualine")  
+require("jake.plugins.telescope")
+require("jake.plugins.nvim-cmp")
 
 vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
 vim.cmd("hi EndOfBuffer guibg=NONE ctermbg=NONE")
 vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+
