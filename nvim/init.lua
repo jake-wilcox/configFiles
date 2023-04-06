34fa4fe5f4731625a263d4d136045ec2d6ebcb4b
Jake Wilcox <jakewilcox242@gmail.com>
2023-04-06 16:02:54 -0700

    nvim lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,6 +7,11 @@ require("jake.plugins.nvim-tree")
 require("jake.plugins.lualine")  
 require("jake.plugins.telescope")
 require("jake.plugins.nvim-cmp")
+require("jake.plugins.lsp.mason")
+require("jake.plugins.lsp.lspsaga")
+require("jake.plugins.lsp.lspconfig")
+
+
 
 vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
 vim.cmd("hi EndOfBuffer guibg=NONE ctermbg=NONE")
--- nvim/lua/jake/plugins/comment.lua
+++ nvim/lua/jake/plugins/comment.lua
@@ -2,5 +2,4 @@ local setup, comment = pcall(require, "Comment")
 if not setup then
     return
 end
-
 comment.setup()
