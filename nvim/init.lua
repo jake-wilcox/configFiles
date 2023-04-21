85b7fc4983c76e58db8ffab5e747444d136ef8a3
Jake Wilcox <jakewilcox242@gmail.com>
2023-04-21 09:03:17 -0700

    autoformat on save nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,4 +16,5 @@ require("jake.plugins.lsp.lspconfig")
 vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
 vim.cmd("hi EndOfBuffer guibg=NONE ctermbg=NONE")
 vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
-
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
